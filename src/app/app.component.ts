daffa31b83ff169529f9f6a8f57ce68c5b364f40
Skyleguy <kylelobsinger@gmail.com>
2022-08-29 11:45:31 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {
+    this.getPdf(this.pdfs[0].source);
+  }
+
   title = 'pdf-viewer-test';
+  pdfs: any[] = [
+    {
+      title: '160F-2019',
+      source: 'assets/160F-2019.pdf',
+    },
+    {
+      title: '1000_Java_Tips_low.copy_1.1',
+      source: 'assets/1000_Java_Tips_low.copy_1.1.pdf',
+    },
+    {
+      title: 'AIDLandlordLeasingAgent',
+      source: 'assets/AIDLandlordLeasingAgent.pdf',
+    },
+    {
+      title: 'pdf-sample',
+      source: 'assets/pdf-sample.pdf',
+    },
+    {
+      title: 'user-experience',
+      source: 'assets/user-experience.pdf',
+    },
+  ];
+  currentSource!: Uint8Array;
+
+  getPdf(source: string) {
+    this.http
+      .get(`http://localhost:4200/${source}`, {
+        headers: {
+          Accept: 'application/pdf',
+        },
+        responseType: 'arraybuffer',
+      })
+      .subscribe({
+        next: (buffer: ArrayBuffer) => {
+          this.currentSource = new Uint8Array(buffer);
+        },
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxExtendedPdfViewerModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
